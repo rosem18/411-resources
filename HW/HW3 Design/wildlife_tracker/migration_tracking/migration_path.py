de1ae6aecac53a06791c3695683e9fedd5f15d05
rosem18 <lingm@bu.edu>
2024-10-08 02:07:43 -0400

    Update migration_path.py

--- HW/HW3 Design/wildlife_tracker/migration_tracking/migration_path.py
+++ HW/HW3 Design/wildlife_tracker/migration_tracking/migration_path.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from wildlife_tracker.migration_path.migration import Migration
+from wildlife_tracker.migration_management.migration_path import MigrationPath
 
 class MigrationPath:
     def __init__(self) -> None:
