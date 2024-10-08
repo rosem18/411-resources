78620d124dc61e9284913fc46336171926d19cc5
rosem18 <lingm@bu.edu>
2024-10-08 01:57:11 -0400

    Update migration_manager.py

--- HW/HW3 Design/wildlife_tracker/migration_tracking/migration_manager.py
+++ HW/HW3 Design/wildlife_tracker/migration_tracking/migration_manager.py
@@ -2,5 +2,33 @@ from typing import Optional
 
 
 class MigrationManager:
+    
+    def __init__(self) -> None:
+        migrations: dict[int, Migration] = {}
 
-    pass
+    def cancel_migration(migration_id: int) -> None:
+        pass
+
+    def get_migration_by_id(migration_id: int) -> Migration:
+        pass
+
+    def get_migration_details(migration_id: int) -> dict[str, Any]:
+        pass
+
+    def get_migrations() -> list[Migration]:
+        pass
+
+    def get_migrations_by_current_location(current_location: str) -> list[Migration]:
+        pass
+
+    def get_migrations_by_start_date(start_date: str) -> list[Migration]:
+        pass
+
+    def get_migrations_by_status(status: str) -> list[Migration]:
+        pass
+
+    def schedule_migration(migration_path: MigrationPath) -> None:
+        pass
+
+    def update_migration_details(migration_id: int, **kwargs: Any) -> None:
+        pass
