7ba9c5546377a77724d10efd206108b560a6d8bc
rosem18 <lingm@bu.edu>
2024-10-08 01:58:26 -0400

    Update migration_path.py

--- HW/HW3 Design/wildlife_tracker/migration_tracking/migration_path.py
+++ HW/HW3 Design/wildlife_tracker/migration_tracking/migration_path.py
@@ -1,5 +1,38 @@
 from typing import Optional
 
 class MigrationPath:
+    def __init__(self) -> None:
+        paths: dict[int, MigrationPath] = {}
 
-    pass
+    def create_migration_path(species: str, start_location: Habitat, destination: Habitat, duration: Optional[int] = None) -> None:
+        pass
+
+    def get_migration_path_by_id(path_id: int) -> MigrationPath:
+        pass
+
+    def get_migration_paths() -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_species(species: str) -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_start_location(start_location: Habitat) -> list[MigrationPath]:
+        pass
+
+    def get_migrations_by_migration_path(migration_path_id: int) -> list[Migration]:
+        pass
+
+    def get_migration_path_details(path_id) -> dict:
+        pass
+
+    def remove_migration_path(path_id: int) -> None:
+        pass
+
+    def schedule_migration(migration_path: MigrationPath) -> None:
+        pass
+
+    def update_migration_path_details(path_id: int, **kwargs) -> None:
+        pass
